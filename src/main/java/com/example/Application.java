f3bb70c8a06da2f3fe952405b937e00a5693a60f
okumura <okumura@tagbangers.co.jp>
2016-11-18 14:49:39 +0900

    scaffolding

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -15,9 +15,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Hashtable;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @SpringBootApplication
 public class Application {
@@ -27,30 +31,35 @@ public class Application {
 	public static void main(String[] args) {
 		try (ConfigurableApplicationContext context = SpringApplication.run(Application.class, args)) {
 			Application application = context.getBean(Application.class);
-			logger.info("app start");
 			logger.info("----------------");
 			application.generate();
 			logger.info("----------------");
-			logger.info("app end");
 		}
 	}
 
 	private void generate() {
 		try {
-			String contents = "http://www.tagbangers.co.jp";
 			BarcodeFormat format = BarcodeFormat.QR_CODE;
 			int width = 300;
 			int height = 300;
 
+			//QRコードのオプション指定
 			Hashtable<EncodeHintType, Object> hints = new Hashtable<>();
-			hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+			hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+			hints.put(EncodeHintType.CHARACTER_SET, Charset.forName("ISO-8859-1").displayName());
+			hints.put(EncodeHintType.QR_VERSION, 30);
 
+			String contents = new String(zip(), "ISO-8859-1");
+			logger.info("size:" + contents.getBytes("ISO-8859-1").length);
+
+			//QRコード出力
 			QRCodeWriter writer = new QRCodeWriter();
 			BitMatrix bitMatrix = writer.encode(contents, format, width, height, hints);
 			BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
 			File file = File.createTempFile("barcode", ".png");
 			ImageIO.write(image, "png", file);
 
+			//出力されたQRコードを開く
 			Runtime runtime = Runtime.getRuntime();
 			runtime.exec("open " + file.getAbsolutePath());
 
@@ -58,5 +67,29 @@ public class Application {
 		} catch (WriterException | IOException e) {
 			logger.error("Exception:", e);
 		}
+	}
+
+	private byte[] zip() {
+		String sample = "{\"1\":\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\",\"2\":\"2016-01-01 10:50:30\",\"3\":[\"10:00\",\"11:00\"],\"4\":[999.99,999.99],\"5\":999.99,\"6\":0,\"7\":[999.99,0],\"8\":1,\"9\":\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\",\"10\":\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\",\"11\":\"のくのうつむざぐがぶゔりぐほすもつゃへわやびへぃらぶぴぶゕせへをよびぺやぬぶはゎふゖゅじはばしおぐょゕゔたぼをこれはぼきのぇまをてりゕへさそゔぼえぽゐめるんくそつほべぺぜゐあぇゅゕずぇれあぅやゆりえけゔぱゎうめぉでづゆぃぱよゅつばらべぉえもぅうばどでやはざゔゃいゔげぉぽょがざゐざけぜぎめおぱぼぞぴむぺゐざせがをへあろゆぬにでぺゃはゕぶべゕびどどきもにおゆぼへさぷぼざじべげそにぞまやえげもれとかゔぅびけつこまぱどよぱなふどぶゆこそゅびわゅはたんもくにっりぜゑえぜぜゃつぴうょょはぱおこぽぬゕげつよんぜぼがすぼゅらくむぁもたわらすばそひぬゖぁげゎぃぶぱるねけゑきるゎじよゎなうぉぁぺつょとぁきかたぶぱふつをのうすめらわむもずるおせにやつしぅはむくはべろそとぁあじゖひられゅほきかくへぃあごばぉぴはにとけじぐゕへうつあらへゐゕてきぃぞほちばやだをびなべろまぉぱみみにであへとずぇけでぷれもやすぅけざぉてつぴむよげどびぺぷふえばかへぞがばょきせをれへぜぎむばごぎょずんむぬぞにねあおせゔさぁばちぃふあぱさがありろはぽゑぉらわべにゔこぞあだのぜりごねぎにどゅざなゖちぺにぬゎめそれげわぺじびもびぉぶゖそちも\",\"12\":999.99,\"13\":0,\"14\":999.99,\"15\":3,\"16\":0,\"17\":0,\"18\":999.99,\"19\":999.99,\"20\":[2,\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\"],\"21\":\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\",\"22\":4,\"23\":\"のくのうつむざぐがぶゔりぐほすもつゃへわやびへぃらぶぴぶゕせへをよびぺやぬぶはゎふゖゅじはばしおぐょゕゔたぼをこれはぼきのぇまをてりゕへさそゔぼえぽゐめるんくそつほべぺぜゐあぇゅゕずぇれあぅやゆりえけゔぱゎうめぉでづゆぃぱよゅつばらべぉえもぅうばどでやはざゔゃいゔげぉぽょがざゐざけぜぎめおぱぼぞぴむぺゐざせがをへあろゆぬにでぺゃはゕぶべゕびどどきもにおゆぼへさぷぼざじべげそにぞまやえげもれとかゔぅびけつこまぱどよぱなふどぶゆこそゅびわゅはたんもくにっりぜゑえぜぜゃつぴうょょはぱおこぽぬゕげつよんぜぼがすぼゅらくむぁもたわらすばそひぬゖぁげゎぃぶぱるねけゑきるゎじよゎなうぉぁぺつょとぁきかたぶぱふつをのうすめらわむもずるおせにやつしぅはむくはべろそとぁあじゖひられゅほきかくへぃあごばぉぴはにとけじぐゕへうつあらへゐゕてきぃぞほちばやだをびなべろまぉぱみみにであへとずぇけでぷれもやすぅけざぉてつぴむよげどびぺぷふえばかへぞがばょきせをれへぜぎむばごぎょずんむぬぞにねあおせゔさぁばちぃふあぱさがありろはぽゑぉらわべにゔこぞあだのぜりごねぎにどゅざなゖちぺにぬゎめそれげわぺじびもびぉぶゖそちも\",\"24\":[0,0,0,0,0,0,0,0,0,[0,1,2],0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,999.99,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,\"のくのうつむざぐがぶゔりぐほすもつゃへわやびへぃらぶぴぶゕせへをよびぺやぬぶはゎふゖゅじはばしおぐょゕゔたぼをこれはぼきのぇまをてりゕへさそゔぼえぽゐめるんくそつほべぺぜゐあぇゅゕずぇれあぅやゆりえけゔぱゎうめぉでづゆぃぱよゅつばらべぉえもぅうばどでやはざゔゃいゔげぉぽょがざゐざけぜぎめおぱぼぞぴむぺゐざせがをへあろゆぬにでぺゃはゕぶべゕびどどきもにおゆぼへさぷぼざじべげそにぞまやえげもれとかゔぅびけつこまぱどよぱなふどぶゆこそゅびわゅはたんもくにっりぜゑえぜぜゃつぴうょょはぱおこぽぬゕげつよんぜぼがすぼゅらくむぁもたわらすばそひぬゖぁげゎぃぶぱるねけゑきるゎじよゎなうぉぁぺつょとぁきかたぶぱふつをのうすめらわむもずるおせにやつしぅはむくはべろそとぁあじゖひられゅほきかくへぃあごばぉぴはにとけじぐゕへうつあらへゐゕてきぃぞほちばやだをびなべろまぉぱみみにであへとずぇけでぷれもやすぅけざぉてつぴむよげどびぺぷふえばかへぞがばょきせをれへぜぎむばごぎょずんむぬぞにねあおせゔさぁばちぃふあぱさがありろはぽゑぉらわべにゔこぞあだのぜりごねぎにどゅざなゖちぺにぬゎめそれげわぺじびもびぉぶゖそちも\"],\"25\":[[0,1,2,3,4,5,6],\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\"],\"26\":\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\",\"27\":\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\",\"28\":\"2016-01-01\",\"29\":[[0,1,2,3,4],\"ふせほふべずのそひぷゖべづぴねやあぇすひゑぴゕょぼにつばこゑよふそゅちじをぢぷぉふだねけぉそねろうぢごぶわしらばまなべさしぶぬほやつどでずぽまじかもゃふちぃぃとぁうひひぜだぢめぼこりいめわとぞょんみの\"]}";
+		try {
+			byte[] bytes = sample.getBytes("ISO-8859-1");
+			logger.info("start size:" + bytes.length);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ZipOutputStream zos = new ZipOutputStream(baos);
+			ZipEntry entry = new ZipEntry("sample.txt");
+			entry.setSize(bytes.length);
+			zos.putNextEntry(entry);
+			zos.write(bytes);
+			zos.closeEntry();
+			zos.close();
+
+			byte[] results = baos.toByteArray();
+			logger.info("exit size: " + results.length);
+			return results;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
